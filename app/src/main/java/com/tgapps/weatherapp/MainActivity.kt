2bbce4e142fd09f8a76b3c6dade61f9e96b26135
Jorge Aldana <jorge.aldana@areamovil.com.co>
2023-01-15 11:42:31 -0500

    [feat]: initializating Google Maps

--- app/src/main/java/com/tgapps/weatherapp/MainActivity.kt
+++ app/src/main/java/com/tgapps/weatherapp/MainActivity.kt
@@ -2,17 +2,34 @@ package com.tgapps.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.tgapps.weatherapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        
+    }
 
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        Log.d(Companion.TAG, "onMapReady: Map Ready")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
